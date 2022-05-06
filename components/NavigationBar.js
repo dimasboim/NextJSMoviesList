1cda03e8f6d4525d63c94e5d2c8fcedc819d104d
Dimas Prasetyo <dimasprasetyo@MacBook-Pro-Dimas-2.local>
2022-05-06 09:37:48 +0700

    pertemuan 2 setelah uts

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,11 +1,11 @@
 import { Navbar,Nav,Container } from "react-bootstrap" 
 const NavigationBar = () => {
     return(
-        <Navbar>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             {/* <h1>Navigasi Testing</h1> */}
             <Navbar.Brand>Jayakarta Movies</Navbar.Brand>
             <Nav>
-                <Nav.Link>Trending</Nav.Link>
+                <Nav.Link >Trending</Nav.Link>
                 <Nav.Link>Superhero</Nav.Link>
             </Nav>
         </Navbar>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from 'next/head' 
+import React, { useRef } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from '../components/Navigationbar';
-import { Container, Row,Col } from 'react-bootstrap';
- 
- 
+import Intro from '../components/Intro';
+import Trending from '../components/Trending';
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
+
+
 export default function Home() {
+  const myRef = useRef(null)
+   const executeScroll = () => scrollToRef(myRef)
+
+
   return (
     <div className='htmlAll'>
       <Head>
@@ -13,20 +20,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='myBG border'>
+      <div className='myBG'>
         <NavigationBar></NavigationBar> 
-        <div className='border intro'>
-          <Container className='text-dark d-flex justify-content-center align-items-center border'>
-            <Row>
-              <Col>
-                  <h1>Review Film Kesukaanmu</h1>
-              </Col>
-            </Row>
-          </Container>
-        </div>
+        <Intro scroll = {executeScroll}></Intro>
       </div>
       
-       
+       <div className='trending' ref={myRef}>
+          <Trending></Trending>
+       </div>
     </div>
   )
 }
